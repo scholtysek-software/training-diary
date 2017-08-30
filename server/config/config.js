7506db8090f870ab1f4c0319e210e5313db46fe4
Patryk Scholtysek <patryk@scholtysek-software.com>
2017-08-30 21:18:05 +0200

    Refactor config files

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,7 @@
+const params = require('./params.json');
+
 const env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TrainingDiary';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TrainingDiaryTest';
+if (env === 'development' || env === 'test') {
+  Object.assign(process.env, params[env]);
 }
