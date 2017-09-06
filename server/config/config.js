15021a0864032d3841da3fa0d6e420234b09bc5b
Patryk Scholtysek <patryk@scholtysek-software.com>
2017-09-06 18:56:51 +0200

    Remove params.json requirement on production env

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
-const params = require('./params.json');
-
 const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || env === 'test') {
+  // eslint-disable-next-line global-require
+  const params = require('./params.json');
   Object.assign(process.env, params[env]);
 }
